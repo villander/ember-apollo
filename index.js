b18707a20a6bac9bac6bd51dc6fc77c6db969d0b
Michael Villander <nousvillander@gmail.com>
2018-01-06 13:39:29 -0200

    Try import apollo-client

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 'use strict';
 
+const mergeTrees = require('broccoli-merge-trees');
+const path = require('path');
+
 module.exports = {
-  name: 'ember-apollo'
+  name: 'ember-apollo',
+
+  treeForAddon (tree) {
+    const apolloClientPath = path.dirname(require.resolve('apollo-client/index.js'))
+    let apolloClientTree = this.treeGenerator(apolloClientPath)
+
+    if (!tree) {
+      return this._super.treeForAddon.call(this, apolloClientTree)
+    }
+
+    const trees = mergeTrees([apolloClientTree, tree], {
+      overwrite: true
+    })
+
+    return this._super.treeForAddon.call(this, trees)
+  }
 };
---
+++ tests/dummy/app/routes/application.js
@@ -0,0 +1,5 @@
+import Route from '@ember/routing/route';
+import { ApolloClient } from 'apollo-client';
+
+export default Route.extend({
+});
